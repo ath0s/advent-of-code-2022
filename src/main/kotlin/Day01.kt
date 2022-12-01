33a441081c745df92ecaee7d090a2682c6a907c0
ath0s <861482+ath0s@users.noreply.github.com>
2022-12-01 09:19:29 +0100

    Day 1

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,58 @@
+import kotlin.io.path.useLines
+
+class Day01 : Day {
+
+    override fun partOne(filename: String, verbose: Boolean): Number {
+        val sums = calculateSums(filename, verbose)
+        return sums.max()
+    }
+
+    override fun partTwo(filename: String, verbose: Boolean): Number {
+        val sums = calculateSums(filename, verbose)
+        val topThree = sums.sortedDescending().take(3)
+        return topThree.sum()
+    }
+
+    private fun calculateSums(filename: String, verbose: Boolean): List<Int> {
+        val groups = filename.asPath().useLines { lines ->
+            buildList {
+                var group = mutableListOf<String>()
+                lines.forEach { line ->
+                    if (line.isBlank()) {
+                        add(group.toList())
+                        group = mutableListOf()
+                    } else {
+                        group += line
+                    }
+                }
+                if (group.isNotEmpty()) {
+                    add(group.toList())
+                }
+            }
+        }
+        val sums = groups.map { group ->
+            group
+                .onEach {
+                    if (verbose) {
+                        println()
+                        print(it)
+                    }
+                }
+                .sumOf { it.toInt() }
+                .also {
+                    if (verbose) {
+                        print("\t$it")
+                        println()
+                    }
+                }
+        }
+        return sums
+    }
+
+    companion object : Day.Main("Day01.txt") {
+
+        @JvmStatic
+        fun main(args: Array<String>) = main()
+
+    }
+}
---
+++ src/test/kotlin/Day01Test.kt
@@ -0,0 +1,4 @@
+class Day01Test : DayTest<Day01>("Day01_test.txt") {
+    override val partOneExpected = 24000
+    override val partTwoExpected = 45000
+}
