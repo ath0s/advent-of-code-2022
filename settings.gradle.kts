cf51e0cc13b01a17d3ee1ca706153a6ab3a2ab7b
Daniel Bergholm <861482+ath0s@users.noreply.github.com>
2023-12-08 16:15:39 +0100

    Remove Gradle scan Added dependency graph

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,1 @@
 rootProject.name = "advent-of-code-2022"
-
-plugins {
-    id("com.gradle.enterprise") version ("3.15.1")
-}
-
-gradleEnterprise {
-    buildScan {
-        if (System.getenv("CI") == "true") {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
